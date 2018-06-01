fd694c7a77fde9aaea5ee28c8ae04c362bc8559e
Rakesh Iyer <riyer>
2018-05-31 22:38:30 -0700

    add top n

--- FindMaximumRepeating.java
+++ FindMaximumRepeating.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class FindMaximumRepeating {
-	long findMaximumRepeating(long ids[], int maxSize) {
+	Map<Long,Long> findMaximumRepeatingMap(long ids[], int maxSize) {
 		Map<Long, Long> map = new HashMap<>();
 
 		for (long id : ids) {
@@ -24,22 +24,60 @@ class FindMaximumRepeating {
 			}
 		}
 
-		long id = Long.MIN_VALUE;
-		long maxValue = Long.MIN_VALUE;
-		for (Map.Entry<Long, Long> entry : map.entrySet()) {
-			if (entry.getValue() > maxValue) {
-				id = entry.getKey();
-				maxValue = entry.getValue();
+		return map;
+	}
+
+	List<Long> topNRepeating(long ids[], int n) {
+		class Entry {
+			Long id;
+			Long value;
+
+			Entry(Long id, Long value) {
+				this.id = id;
+				this.value = value;
+			}
+
+			// reverse comparator to get max heap
+			class EntryComparator implements Comparator<Entry> {
+				public int compare(Entry a, Entry b) {
+					if (b.value > a.value) {
+						return 1;
+					} else if (b.value < a.value) {
+						return -1;
+					} else {
+						return 0;
+					}
+				}
 			}
 		}
 
-		return id;
+		Map<Long,Long> map = findMaximumRepeatingMap(ids, n*5);
+
+		// put the map into heap and remove top n elements.
+		PriorityQueue<Entry> pQueue = new PriorityQueue<>(1, new Entry(0L, 0L).new EntryComparator());
+
+		for (Map.Entry<Long, Long> entry : map.entrySet()) {
+			Entry e  = new Entry(entry.getKey(), entry.getValue());
+			pQueue.add(e);
+		}
+
+		Entry e;
+		int i = 0;
+		List<Long> list = new ArrayList<>();
+		while ((e = pQueue.poll()) != null && i < n) {
+			list.add(e.id);
+			i++;
+		}
+
+		return list;
 	}
 
 	public static void main(String args[]) {
 		FindMaximumRepeating fmr = new FindMaximumRepeating();
-		long ids[] = {};
+		long ids[] = {1,2,3,2,1,3,2,3};
 
-		System.out.println(fmr.findMaximumRepeating(ids, 1));
+		for (Long id : fmr.topNRepeating(ids, 2)) {
+			System.out.println(id);
+		}
 	}
 }
